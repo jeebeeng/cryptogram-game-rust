a06b75c9525909eae0fa87c8be5a2a2a81ea2526
John Bang <jeebeeng@gmail.com>
2021-10-26 15:19:16 -0400

    Create request module and implement get_quote function

---
+++ src/request.rs
@@ -0,0 +1,18 @@
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Deserialize, Debug)]
+pub struct Quote {
+    pub content: String,
+    pub author: String,
+    pub length: u16,
+    pub tags: Vec<String>,
+}
+
+pub async fn get_quote(url: String) -> Result<Quote, Box<dyn Error>> {
+    let response = reqwest::get(url).await?;
+
+    let quote: Quote = response.json().await?;
+
+    Ok(quote)
+}
